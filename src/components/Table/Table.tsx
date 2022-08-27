759b2da679ea830a8bfb60b410413a8a47d7c8e5
ragib70 <ragib.hussain70@gmail.com>
2022-08-27 14:07:02 +0530

    sorting added

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -5,30 +5,82 @@ import TableElement from "../TableElement/TableElement";
 interface TableProps {}
 
 const Table: FC<TableProps> = (props) => {
+  const [newState, setNewState] = useState(true);
+  const [prevState, setPrevState] = useState(false);
+  const [data, setData] = useState(Data);
   return (
     <table className="table">
       <thead>
         <tr>
           <th scope="col">Wallet Address/Name</th>
-          <th scope="col">Total Balance</th>
+          <th
+            role="button"
+            scope="col"
+            onClick={() => {
+              Data.sort(function (a, b) {
+                let keyA = a.totalBalance,
+                  keyB = b.totalBalance;
+                // Compare the 2 dates
+                if (keyA < keyB) return -1;
+                if (keyA > keyB) return 1;
+                return 0;
+              });
+              setData(Data);
+              setPrevState(!prevState);
+              setNewState(!newState);
+            }}
+          >
+            Total Balance
+          </th>
           <th scope="col">Lifetime Value on your App</th>
           <th scope="col">Total Lifetime Value</th>
           <th scope="col">Risks</th>
-          <th scope="col">Labels</th>
+          <th scope="col" className="nav-item dropdown">
+            <a
+              className="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
+              Labels
+            </a>
+            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li>
+                <a className="dropdown-item" href="#">
+                  NFT
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Stake Hodler
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Defi
+                </a>
+              </li>
+            </ul>
+          </th>
         </tr>
       </thead>
       <tbody>
-        {Data.map((ele) => (
-          <TableElement
-            key={ele.id}
-            addr={ele.walletAddress}
-            balance={ele.totalBalance}
-            lifetimeValue={ele.lifeTimeValueApp}
-            tlv={ele.tlv}
-            risks={ele.risks}
-            labels={ele.labels}
-          />
-        ))}
+        {prevState != newState &&
+          data.map((ele) => {
+            return (
+              <TableElement
+                key={ele.id}
+                addr={ele.walletAddress}
+                balance={ele.totalBalance}
+                lifetimeValue={ele.lifeTimeValueApp}
+                tlv={ele.tlv}
+                risks={ele.risks}
+                labels={ele.labels}
+              />
+            );
+          })}
       </tbody>
     </table>
   );
